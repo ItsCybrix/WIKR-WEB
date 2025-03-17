37fca22616d1423f22049de62af723c5b4ba53fa
Cybrix <cybrixtheproot@proton.me>
2025-03-16 23:18:38 -0400

    Now Playing info

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express')
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.static('./public/assets'))
 app.set('views', './public/views');
 
 
+
+
 // Define routes
 
 app.all('/', (req, res)=>{
@@ -16,6 +19,21 @@ app.all('/', (req, res)=>{
 
 app.get('/listen-live', (req, res)=>{
     res.render('liveplayer');
+})
+
+
+app.get('/api/now-playing',(req, res)=>{
+    axios.get('http://ice1.wikrradio.com/status-json.xsl')
+      .then(function (response) {
+        console.log(response.data.icestats.source.title);
+        res.send(response.data.icestats.source.title)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
 })
 
 
