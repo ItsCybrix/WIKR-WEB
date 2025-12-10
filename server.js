8ad051bf66ea71ceb59e614aee573163f865a193
Cybrix <cybrixtheproot@proton.me>
2025-12-09 23:46:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json())
